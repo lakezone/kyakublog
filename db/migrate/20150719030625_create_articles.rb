a85a91cb233295a7363c52edb8ffe6cefdea05b6
Prelang Builder <builder@prelang.com>
2015-07-19 03:06:26 +0000

    Generated model Article with migration

---
+++ db/migrate/20150719030625_create_articles.rb
@@ -0,0 +1,12 @@
+class CreateArticles < ActiveRecord::Migration
+  def change
+    create_table :articles do |t|
+      t.string :title
+      t.string :post
+      t.integer :user_id
+      t.references :user, index: true
+
+      t.timestamps
+    end
+  end
+end
